1b2ed437b29efc86a69efa92657c20353d17f823
Ishtpreet Singh <500061526@stu.upes.ac.in>
2020-05-08 11:25:24 +0530

    Removed mail

--- email/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con = mysql_connect("localhost:3306", "upesc9pa_cert", "Nevertry") or die("Could not connect to MySQL");
-mysql_select_db("upesc9pa_certification",$con) or die("Could not select db");
-?>
